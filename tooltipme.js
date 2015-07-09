a1129866ca9843c602112cbe6827d788b14b724e
AndersonBrantes <andersonbrantes@gmail.com>
2015-07-08 23:59:42 -0300

    permitindo o plugin funcionar para todos os elementos escolhidos

--- tooltipme.js
+++ tooltipme.js
@@ -2,32 +2,40 @@
 ;(function ( $, window, document, undefined ) {
 
     $.fn.toolTipMe = function(options) {
- 
-        var defaults = {
-          
-        };
- 
-        var settings = $.extend( {}, defaults, options );
- 
-        return this.each(function() {
-            $(this).on({
-                mouseenter: showTipMe,
-                mouseleave: hideTipMe
-            });            
+
+        this.each(function(){
+
+            var defaults = {
+                el: this,
+                position: "bottom"
+            };
+
+            var settings = $.extend( {}, defaults, options );
+
+            // return this.each(function() {
+            //     $(this).on({
+            //         mouseenter: showTipMe,
+            //         mouseleave: hideTipMe
+            //     });
+            // });
+
+            var showTipMe = function(event) {
+               $('div.ttm-style').remove();
+               var nome = $(this).attr("data-tooltipme");
+               $('<div class="ttm-style">'+nome+'</div>').appendTo('body');
+
+               //changeTooltipPosition(event);
+            };
+
+            var hideTipMe = function() {
+                $('div.ttm-style').remove();
+            };
+
+            settings.el.on('mouseenter', showTipMe);
+            settings.el.on('mouseleave', hideTipMe);
+
         });
- 
-    }; 
 
-    var showTipMe = function(event) {
-       $('div.ttm-style').remove();
-       var nome = $(this).attr("data-tooltipme");
-       $('<div class="ttm-style">'+nome+'</div>')
-         .appendTo('body');
-       changeTooltipPosition(event);
-    };
-
-    var hideTipMe = function() {
-        $('div.ttm-style').remove();
     };
 
 })( jQuery, window, document );
